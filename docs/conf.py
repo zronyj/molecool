e82564d520302c8bee304d803afc7036fd6967d4
Rony J. Letona <zronyj@gmail.com>
2024-02-07 17:23:06 +0100

    Changing conf

--- docs/conf.py
+++ docs/conf.py
@@ -28,9 +28,9 @@ copyright = ("2024, Marlene Bosquez. Project structure based on the "
 author = 'Marlene Bosquez'
 
 # The short X.Y version
-version = ''
+version = '0.1'
 # The full version, including alpha/beta/rc tags
-release = ''
+release = 'alpha'
 
 
 # -- General configuration ---------------------------------------------------
